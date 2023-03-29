2d3f4837fcdf85ad630d974cd087e940a53d3201
hvdhavietduc <80439464+hvdhavietduc@users.noreply.github.com>
2023-03-29 13:14:15 +0700

    update index.html

---
+++ js/test.js
@@ -0,0 +1,25 @@
+
+
+async function getchart(){
+const ctx = document.getElementById('myChart');
+  var text =await fetch("http://localhost:3000/Datalights")
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+getchart()
